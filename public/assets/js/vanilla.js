3748ab7ae943936050251e6210661b2ba2bf0472
Iago Silva <iagost1@hotmail.com>
2022-07-28 16:32:07 -0300

    Front-End das páginas de Login e Recuperação de senha concluídos!

---
+++ public/assets/js/vanilla.js
@@ -0,0 +1,35 @@
+function confirmDel() {
+    return confirm('Tem certeza que deseja excluir este aluno?');
+}
+
+function confirmExit() {
+    return confirm('Deseja realmente sair da sua conta?');
+}
+
+function openModal () {
+    let modal = document.querySelector('.add-student-section');
+    
+    if (typeof modal == undefined || modal === null)
+    return;
+
+    modal.style.display = "flex";
+}
+
+document.querySelector(".add-button-close").addEventListener("click", function(event){
+    event.preventDefault()
+});
+
+function closeModal () {
+    let modal = document.querySelector('.add-student-section');
+    
+    if (typeof modal == undefined || modal === null)
+    return;
+
+    modal.style.display = 'none';
+}
+
+function closeWarning () {
+    document.querySelectorAll('.home-warning-success, home-warning-error').forEach((item)=>{
+        item.style.display = 'none';
+    });
+}
