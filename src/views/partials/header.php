5056378548b399b7b5106b3fa94fc7587008a5b3
Iago Silva <iagost1@hotmail.com>
2022-08-03 23:39:06 -0300

    Ajuste no layout da página Home e adição dos banners duplos, juntamente com o carrossel de produtos recentes

--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, user-scalable=no">
         <link rel="stylesheet" href="<?=$base?>/assets/css/style.css">
         <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"/>
